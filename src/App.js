7d0310c28b85f1ec24914ca9e58283b44f88de76
christian06santi45-hub <christian06santi45@gmail.com>
2026-01-07 19:37:22 -0600

    push commit -m "Refactor Highlights component to use Highlight UI component"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Nav from "./components/Nav.jsx";
+import Nav from './components/Nav';
+import Landing from './components/Landing';
+import Highlights from './components/Highlights';
+import Featured from './components/Featured';
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
